047e5986bfcfeced5429a38b13a64e14872d6514
mukes137 <t4.cs.soft9@gmail.com>
2023-04-25 12:58:35 +0545

    changes

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #Installing required dependencies
-sudo yum update -y
-sudo yum install -y nodejs npm
+sudo apt update -y
+sudo apt install -y nodejs npm
 sudo npm install -g pm2
 
 #folder that holds our node.js application
